397f2312eebc38da9591c99c39d326061631e468
Tak Tran <contact@tutaktran.com>
2015-01-30 16:14:05 +0000

    Add redraw ability.

--- app/src/app.js
+++ app/src/app.js
@@ -11,29 +11,40 @@ var data = require("../public/data/population-density");
 window.data = data;
 window._ = _;
 
+function redraw(worldPromise, screenWidth, screenHeight, ballDensity, ballOptions) {
+  var allBalls = [];
+
+  worldPromise.then(function(world) {
+    // Generate all the balls
+    allBalls = balls.createByDensity(world, ballDensity, ballOptions);
+
+    // Redraw view
+    $("#viewport").click(function() {
+      _.each(allBalls, function(b) {
+        world.removeBody(b);
+      });
+      $("#viewport").unbind('click');
+
+      redraw(worldPromise, screenWidth, screenHeight);
+    });
+  });
+
+}
+
 $(document).ready(function() {
   var screenWidth = $("body").innerWidth();
   var screenHeight = $(document).innerHeight();
 
   var worldPromise = canvas.init(screenWidth, screenHeight);
 
-  worldPromise.then(function(world) {
-    // Generate all the balls
-    var gbData = _.find(data, {
-      code: "MO"
-    });
-    var density = gbData.value;
-    var allBalls = balls.createByDensity(world, density, {
-      width: screenWidth,
-      height: screenHeight
-    });
-
-    // Refresh view
-    $("#viewport").click(function() {
-      _.each(allBalls, function(b) {
-        world.removeBody(b);
-      });
-      $("#viewport").unbind('click');
-    });
+  var gbData = _.find(data, {
+    code: "MO"
   });
+  var density = gbData.value;
+
+  var ballOptions =  {
+    width: screenWidth,
+    height: screenHeight
+  };
+  redraw(worldPromise, screenWidth, screenHeight, density, ballOptions);
 });
