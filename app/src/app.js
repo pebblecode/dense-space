ceae6d85d9627669978daac0642c0a8497369702
Tak Tran <contact@tutaktran.com>
2015-01-30 15:48:19 +0000

    Abstract into canvas file.

--- app/src/app.js
+++ app/src/app.js
@@ -1,103 +1,39 @@
 "use strict";
 
 var _ = require('lodash');
-var $ = require ('jquery');
+var $ = require('jquery');
+var canvas = require('./lib/canvas');
 
 var balls = require("./lib/balls");
 var data = require("../public/data/population-density");
 
-var init = function() {
-  var el = {
-    viewport: $("#viewport")
-  };
+// Expose for hacking
+window.data = data;
+window._ = _;
 
+$(document).ready(function() {
   var screenWidth = $("body").innerWidth();
   var screenHeight = $(document).innerHeight();
 
-  el.viewport.width(screenWidth);
-  el.viewport.height(screenHeight);
+  var worldPromise = canvas.init(screenWidth, screenHeight);
 
-  Physics(function(world) {
-
-    var viewWidth = screenWidth;
-    var viewHeight = screenHeight;
-
-    var renderer = Physics.renderer('canvas', {
-      id: 'viewport',
-      width: viewWidth,
-      height: viewHeight,
-      meta: false, // don't display meta data
-      styles: {
-        // set colors for the circle bodies
-        'circle' : {
-          strokeStyle: '#351024',
-          lineWidth: 1,
-          fillStyle: '#B1D884',
-          angleIndicator: '#351024'
-        }
-      }
-    });
-
-    // add the renderer
-    world.add( renderer );
-
-    // bounds of the window
-    var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
-
-    // constrain objects to these bounds
-    world.add(Physics.behavior('edge-collision-detection', {
-      aabb: viewportBounds,
-      restitution: 0.5, // Higher = more bouncy
-      cof: 0.99
-    }));
-
+  worldPromise.then(function(world) {
     // Generate all the balls
     var gbData = _.find(data, {
       code: "MO"
     });
     var density = gbData.value;
     var allBalls = balls.createByDensity(world, density, {
-      width: viewWidth,
-      height: viewWidth
+      width: screenWidth,
+      height: screenHeight
     });
-
-    // render on each step
-    world.on('step', function(){
-      world.render();
-    });
-
-    // ensure objects bounce when edge collision is detected
-    world.add( Physics.behavior('body-impulse-response') );
-
-    // add some gravity
-    world.add( Physics.behavior('constant-acceleration') );
-
-    // colliding with each other
-    world.add( Physics.behavior('body-collision-detection') );
-    world.add( Physics.behavior('sweep-prune') ); // optimization for collision
-
-    // subscribe to ticker to advance the simulation
-    Physics.util.ticker.on(function( time, dt ) {
-      world.step( time );
-    });
-
-    // start the ticker
-    Physics.util.ticker.start();
 
     // Refresh view
     $("#viewport").click(function() {
       _.each(allBalls, function(b) {
         world.removeBody(b);
       });
+      $("#viewport").unbind('click');
     });
-
   });
-};
-
-$(document).ready(function() {
-  init();
 });
-
-// Expose for hacking
-window.data = data;
-window._ = _;
