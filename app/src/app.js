2b5ff9d19e36969d2288eefcda776a97c21b4022
Tak Tran <contact@tutaktran.com>
2015-01-30 14:05:34 +0000

    100 balls.

--- app/src/app.js
+++ app/src/app.js
@@ -18,7 +18,44 @@ var init = function() {
     var viewWidth = screenWidth;
     var viewHeight = screenHeight;
 
-    balls.init(world, viewWidth, viewHeight);
+    var renderer = Physics.renderer('canvas', {
+      id: 'viewport',
+      width: viewWidth,
+      height: viewHeight,
+      meta: false, // don't display meta data
+      styles: {
+        // set colors for the circle bodies
+        'circle' : {
+          strokeStyle: '#351024',
+          lineWidth: 1,
+          fillStyle: '#B1D884',
+          angleIndicator: '#351024'
+        }
+      }
+    });
+
+    // add the renderer
+    world.add( renderer );
+
+    // bounds of the window
+    var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
+
+    // constrain objects to these bounds
+    world.add(Physics.behavior('edge-collision-detection', {
+      aabb: viewportBounds,
+      restitution: 0.99,
+      cof: 0.99
+    }));
+
+    for (var i = 0; i < 100; i++) {
+      balls.create(world, {
+        x: Math.random() * viewWidth,
+        y: Math.random() * viewHeight,
+        vx: Math.random(),
+        vy: Math.random(),
+        radius: Math.random() * 50
+      });
+    }
 
     // render on each step
     world.on('step', function(){
--- app/src/lib/balls.js
+++ app/src/lib/balls.js
@@ -1,48 +1,21 @@
 "use strict";
 
 var balls = {
-  init: function(world, viewWidth, viewHeight) {
-    var renderer = Physics.renderer('canvas', {
-      id: 'viewport',
-      width: viewWidth,
-      height: viewHeight,
-      meta: false, // don't display meta data
-      styles: {
-        // set colors for the circle bodies
-        'circle' : {
-          strokeStyle: '#351024',
-          lineWidth: 1,
-          fillStyle: '#B1D884',
-          angleIndicator: '#351024'
-        }
-      }
-    });
+  create: function(world, options) {
+    options = options || {};
+    var x = options.x || 0; // x-coordinate
+    var y = options.y || 0; // y-coordinate
+    var vx = options.vx || 0.2; // velocity in x-direction
+    var vy = options.vy || 0.01; // velocity in y-direction
+    var radius = options.radius || 20;
 
-    // add the renderer
-    world.add( renderer );
-    // render on each step
-    world.on('step', function(){
-      world.render();
-    });
-
-    // bounds of the window
-    var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
-
-    // constrain objects to these bounds
-    world.add(Physics.behavior('edge-collision-detection', {
-      aabb: viewportBounds,
-      restitution: 0.99,
-      cof: 0.99
-    }));
-
-    // add a circle
     world.add(
       Physics.body('circle', {
-        x: 50, // x-coordinate
-        y: 30, // y-coordinate
-        vx: 0.2, // velocity in x-direction
-        vy: 0.01, // velocity in y-direction
-        radius: 20
+        x: x,
+        y: y,
+        vx: vx,
+        vy: vy,
+        radius: radius
       })
     );
   }
